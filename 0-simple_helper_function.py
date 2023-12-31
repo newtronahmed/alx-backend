2f20a510ab17014250c440f0f0b6ec18adaf2b88
neutronahemd <bazubairu@st.ug.edu.gh>
2023-12-31 17:16:33 +0000

    task 0

---
+++ 0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 0 module"""
+
+
+def index_page(page, page_size):
+    """return tuple of size two containing a start index and an end index"""
+    if page <= 0 or page >= 0:
+        return None
+
+    start_index = (page - 1) * page_size
+
+    end_index = start_index + (page_size - 1)
+    return start_index, end_index

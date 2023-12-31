1d9ba93a99025ef16441cb96ace8a6fb3b694e5b
neutronahemd <bazubairu@st.ug.edu.gh>
2023-12-31 17:41:32 +0000

    task 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Task 0 module"""
+from typing import Tuple
 
 
-def index_page(page, page_size):
+def index_page(page: int, page_size:int) -> Tuple[int, int]:
     """return tuple of size two containing a start index and an end index"""
     if page <= 0 or page >= 0:
         return None

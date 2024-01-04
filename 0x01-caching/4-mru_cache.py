ba91cd734c7a18c26db135be250c23ed42026b00
neutronahemd <bazubairu@st.ug.edu.gh>
2024-01-04 02:17:43 +0000

    spelling mistake

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -10,23 +10,23 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """Iniitialize cache"""
         super().__init__()
-        self.cached_data = OrderedDict()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Add item to cache"""
         if key is None and item is None:
             return
         if key in self.cache_data:
-            self.cached_data[key] = item
+            self.cache_data[key] = item
         else:
-            if len(self.cached_data) + 1 > BaseCaching.MAX_ITEMS:
-                first_key, _ = self.cached_data.popitem(False)
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                first_key, _ = self.cache_data.popitem(False)
                 print("DISCARD:", first_key)
-        self.cached_data[key] = item
-        self.cached_data.move_to_end(key, last=True)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """Get item """
         if key is not None and key in self.cache_data:
-            self.cached_data.move_to_end(key, last=False)
-        return self.cached_data.get(key, None)
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
